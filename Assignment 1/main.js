bb6fd0d79a139403a9c9eedac5ba325a3d85be61
justinscannell777 <79175295+justinscannell777@users.noreply.github.com>
2022-02-16 11:34:13 -0800

    Add files via upload

---
+++ Assignment 1/main.js
@@ -0,0 +1,12 @@
+function init(){
+var canvas = document.getElementById("webgl-canvas");
+gl = canvas.getContext("webgl2");
+gl.clearColor(1,0,1,1);
+cone = new Cone(gl, 20);
+render();
+}
+function render(){
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    cone.render();
+}
+window.onload = init;
